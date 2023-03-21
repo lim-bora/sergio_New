45cc5b4d1320f0d44626b9b6d700d674eeca9add
lim-bora <71415158+lim-bora@users.noreply.github.com>
2023-03-21 17:18:56 +0900

    메인페이지 #2

--- Ogam/js/main.js
+++ Ogam/js/main.js
@@ -45,3 +45,54 @@ $(function() {
     $('.sideSectionSwiper .swiper-wrapper a span').eq(1).text('SPRING/SUMMER ‘23 LOOKBOOK');/*두번째 텍스트*/
 
 });
+
+$(function() {
+    /**메인배너 텍스트*/
+    /*
+    - pc/mo 텍스트 각각 기입해주세요.
+    - 첫번째 배너 텍스트는 pc/mo 두개씩 적어주세요.('첫번째 배너 텍스트' 라고 적힌 부분 모두기입)
+    - 2, 3, 4 번째 등 추가배너는 185/190번째 각 복사하여 eq(숫자)만 바꿔주세요. ex.(.eq(2) => 2번째 배너 / .eq(3) => 3번째 배너)
+    */
+    let pcMaintxt = $('.pc_main01 .mainBannerTxt');
+    let moMaintxt = $('.mo_main01 .mainBannerTxt');
+    let pcSubtxt = $('.pc_main01 p');
+    let moSubtxt = $('.mo_main01 p');
+
+    // 메인배너 타이틀
+    //pc 타이틀
+    pcMaintxt.eq(1).text('NEW YEAR SALE'); /*첫번째 배너 텍스트*/
+    $('.pc_main01 a:last-child').find('h3').text('NEW YEAR SALE'); /*첫번째 배너 텍스트*/
+    pcMaintxt.eq(2).text('test test'); /*두번째 배너 텍스트*/
+    pcMaintxt.eq(3).text('test test test'); /*세번째 배너 텍스트*/
+    pcMaintxt.eq(0).text('test test test'); /*세번째 배너 텍스트*/
+
+    //mo 타이틀
+    moMaintxt.eq(1).text('NEW YEAR SALE'); /*첫번째 배너 텍스트*/
+    $('.mo_main01 a:last-child').find('h3').text('NEW YEAR SALE'); /*첫번째 배너 텍스트*/
+    moMaintxt.eq(2).text('test test'); /*두번째 배너 텍스트*/
+    moMaintxt.eq(3).text('test test test'); /*세번째 배너 텍스트*/
+    moMaintxt.eq(0).text('test test test'); /*세번째 배너 텍스트*/
+
+    // 메인배너 서브설명글
+    //pc 서브글
+    pcSubtxt.eq(1).text("세르지오 타키니의 봄 컬렉션은 민감하게 반응하는 트렌트를 지향하기 위해 새로운 균형을 고민했습니다.\n" +
+        "DAMARINDO 라인에서는 편안함과 세련미를 동시에 약속하는 아이템을 선보입니다.");
+    $('.pc_main01 a:last-child').find('p').text("세르지오 타키니의 봄 컬렉션은 민감하게 반응하는 트렌트를 지향하기 위해 새로운 균형을 고민했습니다.\n" +
+        "DAMARINDO 라인에서는 편안함과 세련미를 동시에 약속하는 아이템을 선보입니다.");
+    pcSubtxt.eq(2).text("세르지오 타키니의 봄 컬렉션~~");
+    pcSubtxt.eq(3).text("세르지오 타키니 테스트~~");
+    pcSubtxt.eq(0).text("세르지오 타키니 테스트~~");
+
+    //mo 서브글
+    moSubtxt.eq(1).text("세르지오 타키니의 봄 컬렉션은 민감하게 반응하는\n트렌트를 지향하기 위해 새로운 균형을 고민했습니다.\n" +
+        "DAMARINDO 라인에서는 편안함과 세련미를\n동시에 약속하는 아이템을 선보입니다.");
+    $('.mo_main01 a:last-child').find('p').text("세르지오 타키니의 봄 컬렉션은 민감하게 반응하는\n트렌트를 지향하기 위해 새로운 균형을 고민했습니다.\n" +
+        "DAMARINDO 라인에서는 편안함과 세련미를\n동시에 약속하는 아이템을 선보입니다.");
+    moSubtxt.eq(2).text("세르지오 타키니의 봄 컬렉션~~");
+    moSubtxt.eq(3).text("세르지오 타키니 테스트~~");
+    moSubtxt.eq(0).text("세르지오 타키니 테스트~~");
+
+
+
+
+});
