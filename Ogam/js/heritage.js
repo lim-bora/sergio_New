304bfd9b24ffcf6cf59204b476e6e14c51b75283
lim-bora <71415158+lim-bora@users.noreply.github.com>
2023-03-21 15:23:49 +0900

    해리티지 완료

---
+++ Ogam/js/heritage.js
@@ -0,0 +1,10 @@
+
+// $(window).resize(function(){
+//     let midddleH3 = $(".middleSection .txtBox h3");
+//     if (window.innerWidth > 1024) {  // 다바이스 크기가 480이상일때
+//         midddleH3.text("정통성, 장인정신, 스타일을 대표하는 헤리티지 테니스웨어" + '<br>' + "세르지오 타키니");
+//     } else {
+//         midddleH3.text("정통성, 장인정신, 스타일을 대표하는" + '<br>' + "헤리티지 테니스웨어" + '<br>' + "세르지오 타키니");
+//     }
+//
+// }).resize();
