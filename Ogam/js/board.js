159d5116847798d3b0e2029937f62060efddc2e3
lim-bora <71415158+lim-bora@users.noreply.github.com>
2023-03-25 00:51:55 +0900

    #수정3

---
+++ Ogam/js/board.js
@@ -0,0 +1,10 @@
+
+// $(function() {
+//     //글쓰기페이지 : 관리자답변유무 width조절
+//     let btn = $('.xans-board-write-9 .ec-base-button .gRight');
+//     let btnLengh = btn.children().length;
+//     console.log(btnLengh)
+//     // if(btnLengh.length == 2){
+//     //
+//     // }
+// });
