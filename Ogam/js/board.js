bdd184d8c2e250e52109b3f4071f74dd14e30d8b
lim-bora <71415158+lim-bora@users.noreply.github.com>
2023-03-27 12:13:37 +0900

    #수정5

--- Ogam/js/board.js
+++ Ogam/js/board.js
@@ -1,10 +1,11 @@
 
-// $(function() {
-//     //글쓰기페이지 : 관리자답변유무 width조절
-//     let btn = $('.xans-board-write-9 .ec-base-button .gRight');
-//     let btnLengh = btn.children().length;
-//     console.log(btnLengh)
-//     // if(btnLengh.length == 2){
-//     //
-//     // }
-// });
+$(function() {
+    //1:1문의 글쓰기페이지 관리자답변 유무 : 버튼 그리드 조절
+    let hasBtn = $('.xans-board-write-9 .ec-base-button.gBottom').find('.hasBtn');
+
+    if(hasBtn.hasClass("displaynone") === true) {//관리자답변없으면, 버튼2개면 한줄나열
+        $('.xans-board-write-9 .ec-base-button.gBottom').css('flexDirection','row');
+    } else {
+
+    }
+});
