ac9af2c8c96d961d718905d927e9dab811f8da39
JSY0305 <soyeon.jang@5gamstudio.com>
2023-04-07 16:41:03 +0900

    기획전1 스크립트 작업 완료

--- Ogam/js/shopinfo.js
+++ Ogam/js/shopinfo.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    $("dd").css("display","none")
+    $("dd").css("display","none");
 
     $(".standard_M dl dt").click(function (){
+
         $(this).next("dd").stop().slideToggle(200);
         $(".standard_M dl dt").not(this).next("dd").stop().slideUp(200);
 
-        var membership_img_src=($(this).children("img").attr("src") === "/SkinImg/img/btn_page_down.png")
-            ? "/SkinImg/img/btn_page_up.png"
-            : "/SkinImg/img/btn_page_down.png";
-        $(this).children("img").attr("src",membership_img_src);
-    })
-})
+        $("dt").children("img").attr("src","/SkinImg/img/btn_page_down.png");
+        $(this).children("img").attr("src","/SkinImg/img/btn_page_up.png");
+
+    });
+});
