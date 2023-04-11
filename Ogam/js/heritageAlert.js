4513becb4a05c4cb5d057dddec47f9a786554b99
lim-bora <71415158+lim-bora@users.noreply.github.com>
2023-04-11 17:18:55 +0900

    추가수정들 오늘끄읕

---
+++ Ogam/js/heritageAlert.js
@@ -0,0 +1,8 @@
+//오픈전 해리티지 이미지수급 늦어짐으로 얼럿처리 2023.04.11
+//해리티지 완료시 해당코드 제거&주석처리
+$(document).ready(function() {
+    $('.alertHT').click(function(a){
+        a.preventDefault();
+       alert("준비중입니다.");
+    });
+})
