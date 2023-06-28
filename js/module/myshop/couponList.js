1b20ec95e25983b06303ae79aa1c0d0f31753cdd
lim-bora <71415158+lim-bora@users.noreply.github.com>
2023-06-28 13:36:06 +0900

    쿠폰함 카운트/컴마추가

--- js/module/myshop/couponList.js
+++ js/module/myshop/couponList.js
@@ -17,4 +17,50 @@
             prchsAmnt.text(prchsAmntRe);
         });
     });
+
+    //반복 : 사용가능기간 카운트형식으로 변경
+    $('.couponList tbody').each(function(){
+        let $tbody = $(this),
+            $period = $tbody.find('.usePeriod .content'),
+            period = $period.text(),
+            replacePeriod = period.replaceAll('-', '.');
+
+        getTime(replacePeriod, $period);
+    });
+
+    //반복 : 쿠폰혜택 컴마 추가
+    $('.couponList tbody').each(function(){
+        let $tbody = $(this),
+            $benefit = $tbody.find('.cpBenefit .content'),
+            benefit = $benefit.text();
+        $benefit.text(comma(benefit))
+    });
 })();
+
+/*******디데이 카운트계산*********/
+function getTime(date, $el){
+    let cnt = date;
+    let strSlice = cnt.substring(0, 19); //시작날짜 커트
+    let ddaySlice = cnt.substring(22, 32); //마지막날짜 커트
+    const Dday = new Date(ddaySlice); //마지막날짜
+    const today = new Date(); //오늘날짜
+    const diff = Dday.getTime() - today.getTime(), //마지막날짜 - 오늘날짜(남은날짜)
+        secInMs = Math.floor(diff / 1000), //남은날짜 / 1000 = 초단위로 변환
+        minInMs = Math.floor(secInMs / 60), //남은초단위 / 60 = 분단위로 변환
+        hourInMs = Math.floor(minInMs / 60), //남은분단위 / 60 = 시간단위로 변환
+        days = Math.floor(hourInMs / 24); //남은시간단위 / 24 = 날짜로 변환
+
+    //console.log(`${days}일 이내 (${strSlice} ~ ${ddaySlice})`);
+    $el.text(`~${ddaySlice} (${days}일 남음)`);
+}
+
+/*******쿠폰혜택 컴마추가*********/
+function comma(str){
+    try {
+        str = String(str);
+        return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
+    } catch (err) {
+        console.log(err);
+        return str;
+    }
+}
