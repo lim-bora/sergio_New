6d94338e6335a2648420afec96d6e5401aebee20
이주연 <45332354+1uyeon@users.noreply.github.com>
2023-06-30 15:19:05 +0900

    7/7 기획전 스크립트 수정

--- Ogam/js/project.js
+++ Ogam/js/project.js
@@ -92,26 +92,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
             prevEl: ".swiper-button-prev",
         },
     });
-
-    // 230707 기획전
-    var containerWidth = document.querySelector('.timeless_project .og_product_here .prdList__item').offsetWidth;
-    var slideWidth = containerWidth * 0.5;
-    const time_swiper = new Swiper(".timeSwiper", {
-        slidesPerView: 'auto',
-        loop: true,
-        slidesOffsetBefore: slideWidth,
-        centeredSlides: true,
-        watchOverflow : true,
-        resistance : false,
-        autoplay: {
-            delay: 2000,
-        },
-        speed: 800,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
 });
 
 
