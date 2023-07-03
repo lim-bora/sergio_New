e6c9071e8024307298ca49bbd35c59761800d7a1
이주연 <45332354+1uyeon@users.noreply.github.com>
2023-07-03 15:04:50 +0900

    7/7 기획전 mo 작업완료

--- project/js/timeless_230707.js
+++ project/js/timeless_230707.js
@@ -2,20 +2,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // 230707 기획전
     var containerWidth = document.querySelector('.og_product_here .prdList__item').offsetWidth;
     var slideWidth = containerWidth * 0.5;
-    const time_swiper = new Swiper(".timeSwiper", {
+    const time_swiper = new Swiper(".pc_swiper .timeSwiper", {
         slidesPerView: 'auto',
         loop: true,
         slidesOffsetBefore: slideWidth,
         centeredSlides: true,
         watchOverflow : true,
         resistance : false,
+
         autoplay: {
             delay: 2000,
+            disableOnInteraction: false,
         },
         speed: 800,
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+
+    });
+
+    const time_swiper_mo = new Swiper(".mo_swiper .timeSwiper", {
+        slidesPerView: 1.5,
+
     });
 });
